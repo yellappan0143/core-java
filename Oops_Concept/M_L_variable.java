9f01f43ec998673069f08189893aa39573412640
YELLAPPAN P <109733281+Madesh0143@users.noreply.github.com>
2023-09-04 22:12:59 +0530

    Update M_L_variable.java

--- Oops_Concept/M_L_variable.java
+++ Oops_Concept/M_L_variable.java
@@ -1,68 +1,62 @@
-// // IF DATA MEMBER AND FUNCTION MEMBER ASSIGNED BY "STATIC" AND "STATIC" ONLY ACCESING MEMBERNAME
+// 1. IF DATA MEMBER AND FUNCTION MEMBER ASSIGNED BY "STATIC" AND "STATIC" ONLY ACCESING MEMBERNAME
 
-// package OOPS;
+package OOPS;
 
-// public class M_L_variable 
-// {
+public class M_L_variable 
+{
+    // DATA MEMBER IS STATIC
+    static int A = 500;        // STATIC PRIMITIVE VARIABLES
+    static String Sub;        // NON-STATIC REFERENCE VARIABLES
 
-//     // DATA MEMBER IS STATIC
-//     static int A = 500;        // STATIC PRIMITIVE VARIABLES
-//     static String Sub;        // NON-STATIC REFERENCE VARIABLES
+    // FUNCTION MEMBER OR METHOD IS STATIC
+    public static void test() 
+   {
+        int A =100;             // NON-STATIC LOCAL PRIMITIVE VARIABLES
+        String Sub = "SQL";    // NON-STATIC LOCAL REFERENCE VARIABLES
+
+        System.out.println(A);
+        System.out.println(Sub);
+
+        System.out.println(M_L_variable.A);    // CALLING FROM DATA MEMBER
+        System.out.println(M_L_variable.Sub);  // CLASSNAME.MEMBERNAME         
+    }
+    public static void main(String[] args) 
+    {
+        test();
+    }
+}
 
 
-//     // FUNCTION MEMBER OR METHOD IS STATIC
-//     public static void test() 
-//{
-//         int A =100;             // NON-STATIC LOCAL PRIMITIVE VARIABLES
-//         String Sub = "SQL";    // NON-STATIC LOCAL REFERENCE VARIABLES
+// 2. IF DATA MEMBER AND FUNCTION MEMBER ASSIGNED BY "NON-STATIC" AND "STATIC" WITH THE HELP OF CREATING "OBJECT"
 
-//         System.out.println(A);
-//         System.out.println(Sub);
+package OOPS;
 
-//         System.out.println(M_L_variable.A);    // CALLING FROM DATA MEMBER
-//         System.out.println(M_L_variable.Sub);  // CLASSNAME.MEMBERNAME
-       
+public class M_L_variable 
+{
+    // DATA MEMBER IS NON-STATIC
+     int A = 500;        // NON-STATIC PRIMITIVE VARIABLES
+     String Sub;        // NON-STATIC REFERENCE VARIABLES
 
+    // FUNCTION MEMBER OR METHOD IS STATIC
+    public static void test()
+       {
+        int A =100;             // NON-STATIC LOCAL PRIMITIVE VARIABLES
+        String Sub = "SQL";    // NON-STATIC LOCAL REFERENCE VARIABLES
+
+        System.out.println(A);
+        System.out.println(Sub);
+
+        System.out.println(new M_L_variable().A);    // ACCESSING THE DATA FORM DATA MEMBER 
+        System.out.println(new M_L_variable().Sub);  //  NEW CLASSNAME().MEMBERNAME
         
-//     }
+       }
+    public static void main(String[] args) 
+    {
+        test();
+    }
+}
 
-//     public static void main(String[] args) {
-//         test();
-
-//     }
-// }
-
-
-// // IF DATA MEMBER AND FUNCTION MEMBER ASSIGNED BY "NON-STATIC" AND "STATIC" WITH THE HELP OF CREATING "OBJECT"
-
-// package OOPS;
-
-// public class M_L_variable 
-// {
-//     // DATA MEMBER IS NON-STATIC
-//      int A = 500;        // NON-STATIC PRIMITIVE VARIABLES
-//      String Sub;        // NON-STATIC REFERENCE VARIABLES
-
-//     // FUNCTION MEMBER OR METHOD IS STATIC
-//     public static void test() {
-//         int A =100;             // NON-STATIC LOCAL PRIMITIVE VARIABLES
-//         String Sub = "SQL";    // NON-STATIC LOCAL REFERENCE VARIABLES
-
-//         System.out.println(A);
-//         System.out.println(Sub);
-
-//         System.out.println(new M_L_variable().A);    // ACCESSING THE DATA FORM DATA MEMBER 
-//         System.out.println(new M_L_variable().Sub);  //  NEW CLASSNAME().MEMBERNAME
-        
-//     }
-
-//     public static void main(String[] args) {
-//         test();
-
-//     }
-// }
-
-// IF DATA MEMBER AND FUNCTION MEMBER ASSIGNED BY "STATIC" AND "NON-STATIC" WITH THE HELP OF CREATING "OBJECT"
+// 3. IF DATA MEMBER AND FUNCTION MEMBER ASSIGNED BY "STATIC" AND "NON-STATIC" WITH THE HELP OF CREATING "OBJECT"
 
 package OOPS;
 
@@ -70,10 +64,11 @@ public class M_L_variable
 {
     // DATA MEMBER IS STATIC
      static int A = 500;        // STATIC PRIMITIVE VARIABLES
-    static String Sub;        // STATIC REFERENCE VARIABLES
+     static String Sub;        // STATIC REFERENCE VARIABLES
 
     // FUNCTION MEMBER OR METHOD IS NON-STATIC
-    public  void test() {
+    public  void test()
+    {
         int A =100;             // NON-STATIC LOCAL PRIMITIVE VARIABLES
         String Sub = "SQL";    // NON-STATIC LOCAL REFERENCE VARIABLES
 
@@ -81,45 +76,40 @@ public class M_L_variable
         System.out.println(Sub);
 
         System.out.println(this.A);    // ACCESSING THE DATA FORM DATA MEMBER 
-        System.out.println(this.Sub);  //  THIS.MEMBERNAME / NEW CLASSNAME.MEMBERNAME
-        
+        System.out.println(this.Sub);  //  THIS.MEMBERNAME / NEW CLASSNAME.MEMBERNAME      
     }
-
-    public static void main(String[] args) { 
+    public static void main(String[] args) 
+    { 
         // BY CREATING THE OBJECT IN MAIN METHOD IS NEW CLASSNAME.MEMBERNAME
-
         new M_L_variable().test();
-
     }
 }
 
-// // IF DATA MEMBER AND FUNCTION MEMBER ASSIGNED BY "NON-STATIC" AND "NON-STATIC" WITH THE HELP OF CREATING "OBJECT"
+// 4. IF DATA MEMBER AND FUNCTION MEMBER ASSIGNED BY "NON-STATIC" AND "NON-STATIC" WITH THE HELP OF CREATING "OBJECT"
 
-// package OOPS;
+package OOPS;
 
-// public class M_L_variable 
-// {
-//     // DATA MEMBER IS NON-STATIC
-//       int A = 500;        // NON-STATIC PRIMITIVE VARIABLES
-//       String Sub;        // NON-STATIC REFERENCE VARIABLES
+public class M_L_variable 
+{
+    // DATA MEMBER IS NON-STATIC
+      int A = 500;        // NON-STATIC PRIMITIVE VARIABLES
+      String Sub;        // NON-STATIC REFERENCE VARIABLES
 
-//     // FUNCTION MEMBER OR METHOD IS NON-STATIC
-//     public  void test() {
-//         int A =100;             // NON-STATIC LOCAL PRIMITIVE VARIABLES
-//         String Sub = "SQL";    // NON-STATIC LOCAL REFERENCE VARIABLES
+    // FUNCTION MEMBER OR METHOD IS NON-STATIC
+    public  void test() 
+    {
+        int A =100;             // NON-STATIC LOCAL PRIMITIVE VARIABLES
+        String Sub = "SQL";    // NON-STATIC LOCAL REFERENCE VARIABLES
 
-//         System.out.println(A);
-//         System.out.println(Sub);
+        System.out.println(A);
+        System.out.println(Sub);
 
-//         System.out.println(this.A);    // ACCESSING THE DATA FORM DATA MEMBER 
-//         System.out.println(this.Sub);  //  THIS.MEMBERNAME
-        
-//     }
-
-//     public static void main(String[] args) { 
-//         // BY CREATING THE OBJECT IN MAIN METHOD IS NEW CLASSNAME.MEMBERNAME
-
-//         new M_L_variable().test();
-
-//     }
-// }
+        System.out.println(this.A);    // ACCESSING THE DATA FORM DATA MEMBER 
+        System.out.println(this.Sub);  //  THIS.MEMBERNAME       
+    }
+    public static void main(String[] args) 
+    { 
+        // BY CREATING THE OBJECT IN MAIN METHOD IS NEW CLASSNAME.MEMBERNAME
+        new M_L_variable().test();
+    }
+}
