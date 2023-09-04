351c0e8f82a5a8b76745c72a94d1daafff27fcb9
YELLAPPAN P <109733281+Madesh0143@users.noreply.github.com>
2023-09-04 22:30:38 +0530

    Add files via upload

---
+++ Operators/A_operatora.java
@@ -0,0 +1,12 @@
+class A_operatora {
+   public static void main(String[] args) {
+    double P,T,R,Result;
+    P=82.0;
+    T=128.1;
+    R=450.82;
+    Result=P*T*R/100;
+
+    System.out.println(Result);
+
+   } 
+}
